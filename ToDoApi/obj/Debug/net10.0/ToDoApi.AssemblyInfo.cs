44226bd42865d94b8b6c70f14ca1accf31e7d1da
Chavi Carelbach <cc5379417@gmail.com>
2026-04-22 18:17:43 +0300

    add Dockerfile

--- ToDoApi/obj/Debug/net10.0/ToDoApi.AssemblyInfo.cs
+++ ToDoApi/obj/Debug/net10.0/ToDoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19f7201a518424251b21cae6d187ba4cae412a75")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
